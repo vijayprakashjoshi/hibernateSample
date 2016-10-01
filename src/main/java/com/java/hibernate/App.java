c125988960c89c728de9e627c6b9b793be800839
Vijay Prakash Joshi <joshi.vijay03@gmail.com>
2016-10-01 12:29:14 +0530

    Hibernate Tutorial for JDBC Connection using mysql

---
+++ src/main/java/com/java/hibernate/App.java
@@ -0,0 +1,13 @@
+package com.java.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
